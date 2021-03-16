d3c0e9a61de547519c9d01e372debaff6191a3ff
jaredtovar4172 <80796166+jaredtovar4172@users.noreply.github.com>
2021-03-16 17:47:12 -0600

    Add files via upload

---
+++ Ej5_TrianguloHipotenusa/Ej5_TrianguloHipotenusa/Calculo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej5_TrianguloHipotenusa
+{
+    class Calculo
+    {
+        public double Lado (double a, double b)
+        {
+            double h = (double)Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+            return (h);
+        }
+    }
+}
---
+++ Ej5_TrianguloHipotenusa/Ej5_TrianguloHipotenusa/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ej5_TrianguloHipotenusa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Calculo x = new Calculo();
+            double cateto1 = double.Parse(textBox1.Text);
+            double cateto2 = double.Parse(textBox2.Text);
+            double h = x.Lado(cateto1, cateto2);
+            this.textBox3.Text = h.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox1.Focus();
+            textBox2.Clear();
+            textBox2.Focus();
+            textBox3.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult salir;
+            salir = MessageBox.Show("Deseas salir de la aplicacion?", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (salir == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+    }
+}
+    
